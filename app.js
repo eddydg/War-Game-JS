8ca506a1d99290a69c41aed762fd0d31f1c04431
Eddydg <eddydg@users.noreply.github.com>
2016-10-29 01:08:44 +0200

    Even more compact

--- app.js
+++ app.js
@@ -20,48 +20,38 @@ var cardsp1 = [...Array(parseInt(readline()))].map(_=> readline());
 var cardsp2 = [...Array(parseInt(readline()))].map(_=> readline());
 
 function playWar(cardp1, cardp2, p1stack = [], p2stack = []) {
-
     if (cardsp1.length < 4 || cardsp2.length < 4) {
         print('PAT'); return true;
     }
 
     p1stack = p1stack.concat(cardp1, cardsp1.splice(0, 3));
     p2stack = p2stack.concat(cardp2, cardsp2.splice(0, 3));
-
-    cardp1 = cardsp1.shift();
-    cardp2 = cardsp2.shift();
-
+        
+    [cardp1, cardp2] = [cardsp1.shift(), cardsp2.shift()];
+    
     if (getFightWinner(cardp1, cardp2) === 0)
         return playWar(cardp1, cardp2, p1stack, p2stack);
-
-    let winnerStack = p1stack.concat(cardp1, p2stack, cardp2);
-    if (getFightWinner(cardp1, cardp2) === 1) {
-        cardsp1 = cardsp1.concat(winnerStack);
-    } else {
-        cardsp2 = cardsp2.concat(winnerStack);
-    }
-
+    if (getFightWinner(cardp1, cardp2) === 1)
+        cardsp1 = cardsp1.concat(p1stack, cardp1, p2stack, cardp2);
+    else
+        cardsp2 = cardsp2.concat(p1stack, cardp1, p2stack, cardp2);
+    
     return false;
 }
 
-for (let round = 0;; round++) {
-
+for (round = 0;; round++) {
     if (cardsp1.length === 0 && cardsp2.length === 0) {
         print('PAT'); break;
     } else if (cardsp1.length === 0) {
         print('2 ' + round); break;
     } else if (cardsp2.length === 0) {
-        print('1 ' + round); break;
+        print('1 ' + round); break;   
     }
 
-    let cardp1 = cardsp1.shift();
-    let cardp2 = cardsp2.shift();
-
+    [cardp1, cardp2] = [cardsp1.shift(), cardsp2.shift()];
     if (getFightWinner(cardp1, cardp2) === 1) {
         cardsp1.push(cardp1, cardp2);
     } else if (getFightWinner(cardp1, cardp2) === -1) {
         cardsp2.push(cardp1, cardp2);
-    } else if (playWar(cardp1, cardp2)) {
-        break;
-    }
+    } else if (playWar(cardp1, cardp2)) break;
 }
